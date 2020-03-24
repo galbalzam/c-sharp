5419bcfb36500dc81a9fbcacfe14b6ad16cd709d
galbalzam <62279702+galbalzam@users.noreply.github.com>
2020-03-24 12:14:24 +0200

    Add files via upload

---
+++ תרגיל כיתה c#/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace lesson_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lengthOfArray = Convert.ToInt32(Console.ReadLine());
+            int NumToMul = Convert.ToInt32(Console.ReadLine());
+
+            int[]arr= new int[lengthOfArray];
+
+            Console.WriteLine($"Num of elemet: {lengthOfArray},num to mul:{NumToMul}");
+            
+            for(int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = i * NumToMul;
+                Console.Write(arr[i]+ ", ");
+            }
+
+    
+            Console.ReadKey();
+        }
+
+    }
+}
