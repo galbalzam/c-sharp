bb252004554be97f52c7e09d0e9b7858355d7fa4
galbalzam <62279702+galbalzam@users.noreply.github.com>
2020-05-05 12:20:12 +0300

    Add files via upload

---
+++ תרגיל כיתה c#/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Matala3Q1
+{
+    class cookies
+    {
+        private Boolean gluten;
+        public Boolean Gluten
+        {
+            get { return gluten; }
+            set { gluten = value; }
+        }
+
+        private int numOfEggs;
+
+        public int NumOfEggs
+        {
+            get { return numOfEggs; }
+            set { numOfEggs = value; }
+        }
+
+
+        private float numOfFlour;
+        public float NumOfFlour
+        {
+            get { return numOfFlour; }
+            set { numOfFlour = value; }
+
+        }
+
+
+        private float numOfSugar;
+        public float NumOfSugar
+        {
+            get { return numOfSugar; }
+            set { numOfSugar = value; }
+        }
+
+    }
+}
