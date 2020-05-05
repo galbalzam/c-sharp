4b5bac91629e6b6bd3b78743748e4f342d1ee8d7
galbalzam <62279702+galbalzam@users.noreply.github.com>
2020-05-05 17:23:13 +0300

    Add files via upload

---
+++ תרגיל כיתה c#/Car.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Matala
+{
+    class Car
+    {
+        private int speed = 0;
+        
+        public int Speed
+        {
+            get { return speed; }
+            set { speed = value; }
+        }
+
+        public void upSpeed()
+        {
+            speed = speed + 1;
+            // speed += 1;
+            // speed ++
+        }
+
+        public void downSpeed()
+        {
+            speed--;
+        }
+        public void stop()
+        {
+            speed = 0;
+        }
+        public void printSpeed()
+        {
+            Console.WriteLine(speed);
+        }
+
+    }
+}
--- תרגיל כיתה c#/Program.cs
+++ תרגיל כיתה c#/Program.cs
@@ -1,27 +1,88 @@
 ﻿using System;
 
-namespace lesson_02
+
+namespace Matala
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            int lengthOfArray = Convert.ToInt32(Console.ReadLine());
-            int NumToMul = Convert.ToInt32(Console.ReadLine());
-
-            int[]arr= new int[lengthOfArray];
-
-            Console.WriteLine($"Num of elemet: {lengthOfArray},num to mul:{NumToMul}");
             
-            for(int i = 0; i < arr.Length; i++)
-            {
-                arr[i] = i * NumToMul;
-                Console.Write(arr[i]+ ", ");
-            }
+            cookies c1 = new cookies();
+            c1.Gluten = true;
+            c1.NumOfEggs = 5;
+            c1.NumOfFlour = 5.2f;
+            c1.NumOfSugar = 1.5f;
 
-    
+            cookies c2 = new cookies();
+            c2.Gluten = false;
+            c2.NumOfEggs = 2;
+            c2.NumOfFlour = 7.2f;
+            c2.NumOfSugar = 4.5f;
+
+
+            Console.WriteLine(printDetails(c1));
+            Console.WriteLine(printDetails(c2));
+
+            c1.NumOfSugar = 9.6f;
+            Console.WriteLine(printDetails(c1));
+            Console.WriteLine(printDetails(c2));
+            c1 = c2;
+
+            c2.NumOfEggs = 7;
+            Console.WriteLine(printDetails(c1));
+            Console.WriteLine(printDetails(c2));
+            
+
+            // Question 2
+            // 2.1
+            Console.WriteLine("Create Car: ");            
+            Car car = new Car();
+            // 2.2
+            Console.WriteLine("Print Start Details: ");
+            car.printSpeed();
+            // 2.3
+            Console.WriteLine("Enter Speed You Want Drive: ");
+            int spd = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < spd; i++)
+            {
+                car.upSpeed();
+            }
+            // 2.4
+            Console.WriteLine("The Speed is: ");
+            car.printSpeed();
+            // 2.5
+            Console.WriteLine("Down Speed 2KMH:");
+            car.downSpeed(); car.downSpeed();
+            // 2.6
+            Console.WriteLine("The Speed is: ");
+            car.printSpeed();
+            Console.WriteLine("Stop Car:");
+            car.stop();
+            car.printSpeed();
+
+
             Console.ReadKey();
         }
 
-    }
+
+        
+            public static string printDetails(cookies c)
+            {
+                {
+                    string res = "Details of cookie";
+                    res += $"Gloten:{c.Gluten } \n";
+                    res += $"NumOfSugar:{c.NumOfSugar}\n";
+                    res += $"NumOfEggs:{c.NumOfEggs}\n";
+                    res += $"NumOfFlour:{c.NumOfFlour}\n";
+
+                    return res;
+                }
+            }
+
+            
+            
+        }
+    
 }
